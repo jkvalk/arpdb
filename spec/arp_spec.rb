d5bb86ad26b1168c723a7c58d5c5506c74f3c516
J.K. Valk <jkvalk@mail.ru>
2015-06-18 11:57:57 +0300

    added tests with mocked SNMPTransport objects

--- spec/arp_spec.rb
+++ spec/arp_spec.rb
@@ -1,9 +1,45 @@
 require 'rspec'
 
+
 describe 'Arp' do
 
-  it 'should initialize' do
+  before(:each) do
+    @snmp_transport = instance_double('SNMPTransport', host: '127.0.0.1')
+    allow(@snmp_transport).to receive(:get).and_return('TSaare')
+    allow(@snmp_transport).to receive(:walk).and_return(
+                      [
+                          %w(aa11cc11ee11 10.10.10.1),
+                          %w(aa10bb11cc12 10.10.10.2),
+                          %w(ff10ee20dd30 10.10.10.3)
+                      ]
+                  )
 
-    expect(true).to eq(true)
+    @snmp_transports_many = [] << @snmp_transport << @snmp_transport
+
+  end
+
+  it 'should initialize and scan' do
+
+    expect(Arp.new(@snmp_transports_many).scan).to be_an_instance_of(Arp)
+  end
+
+  it 'should convert mac to ip' do
+    expect(Arp.new(@snmp_transports_many).scan.mac_to_ip('aa11cc11ee11')).to eq('10.10.10.1')
+  end
+
+  it 'should convert ip to mac' do
+    expect(Arp.new(@snmp_transports_many).scan.ip_to_mac('10.10.10.1')).to eq('aa11cc11ee11')
+  end
+
+  it 'should locate ip' do
+    expect(Arp.new(@snmp_transports_many).scan.locate_ip('10.10.10.1')).to eq('TSaare')
+  end
+
+  it 'should locate mac' do
+    expect(Arp.new(@snmp_transports_many).scan.locate_mac('aa11cc11ee11')).to eq('TSaare')
+  end
+
+  it 'should rescan' do
+    expect(Arp.new(@snmp_transports_many).scan.rescan).to be_an_instance_of(Arp)
   end
 end
