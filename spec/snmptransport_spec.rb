f323f70e89c9cf8a2779f50be638e08023423953
J.K.Valk <jkvalk@mail.ru>
2015-06-20 14:27:59 +0300

    Tests for SNMPTransport

--- spec/snmptransport_spec.rb
+++ spec/snmptransport_spec.rb
@@ -2,31 +2,34 @@ require 'rspec'
 
 describe 'SNMPTransport' do
 
-  before(:each) do
-    @snmp_transport = instance_double('SNMPTransport', host: '127.0.0.1')
-    allow(@snmp_transport).to receive(:get).and_return('TSaare')
-    allow(@snmp_transport).to receive(:walk).and_return(
-                                  [
-                                      %w(aa11cc11ee11 10.10.10.1),
-                                      %w(aa10bb11cc12 10.10.10.2),
-                                      %w(ff10ee20dd30 10.10.10.3)
-                                  ]
-                              )
+  before do
+
+    @fake_manager = double(SNMP::Manager)
+    allow(SNMP::Manager).to receive(:new).and_return(@fake_manager)
+
+    @fake_varbind = double(SNMP::VarBind, name: '1.2.3.4.5.6.7.8.9', value: 'MyLocation')
+    #allow(@fake_varbind).to receive(:value).and_return('')
+
+    @fake_varbind_list = double(SNMP::VarBindList)
+    allow(@fake_varbind_list).to receive(:each_varbind).and_yield(@fake_varbind)
+    allow(@fake_varbind_list).to receive(:each).and_yield(@fake_varbind)
+
+    
+    allow(@fake_manager).to receive(:get).and_return(@fake_varbind_list)
+
+    allow(@fake_manager).to receive(:walk).and_yield(@fake_varbind_list)
+    
 
 
-  end
-  it 'should walk' do
 
-    expect(@snmp_transport.walk(%w(1.2.3.4 5.6.7.8))).to eq(
-                                                             [
-                                                                 %w(aa11cc11ee11 10.10.10.1),
-                                                                 %w(aa10bb11cc12 10.10.10.2),
-                                                                 %w(ff10ee20dd30 10.10.10.3)
-                                                             ]
-                                                         )
+
   end
 
   it 'should get' do
-    expect(@snmp_transport.get('1.2.3.4')).to eq('TSaare')
+    expect(SNMPTransport.new('host', 'comm').get('1.2.3.4')).to eq('MyLocation')
+  end
+
+  it 'should walk' do
+    SNMPTransport.new('host', 'comm').walk(%w(1.3.6.1.2.1.4.22.1.2 1.3.6.1.2.1.4.22.1.3))
   end
 end
